c4620f5a57a47ff4627c5af450155a20e2880096
Çağan Mert İŞLEK <islekcaganmert@gmail.com>
2024-05-20 18:05:42 +0300

    query support

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ TARGET_SERVER = 'https://islekcaganmert.pythonanywhere.com'
 @app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 @app.route('/', defaults={'path': ''})
 def proxy(path):
+    query = '?'
+    for i in request.args:
+        query += (i + '=' + request.args[i] + '&')
+    query.removesuffix('&')
+    query.removesuffix('?')
     response = requests.request(
         method=request.method,
-        url=f"{TARGET_SERVER}/{path}",
+        url=f"{TARGET_SERVER}/{path}{query}",
         headers={key: value for key, value in request.headers if key != 'Host'},
         data=request.get_data(),
         cookies=request.cookies,
